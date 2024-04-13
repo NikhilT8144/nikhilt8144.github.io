472798c1543d5ab9040a50f0bd382faa5521452d
Nikhil Tiwari <87422608+NikhilT8144@users.noreply.github.com>
2024-04-13 19:18:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// Disable right-click
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+});
 
+// Disable Ctrl+Shift+I
+document.addEventListener('keydown', function(event) {
+    if (event.ctrlKey && event.shiftKey && event.keyCode === 73) {
+        event.preventDefault();
+    }
+});
