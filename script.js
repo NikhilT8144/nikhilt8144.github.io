404ab87c5e3185a6ded0924eedd1190250a3e927
Nikhil Tiwari <87422608+NikhilT8144@users.noreply.github.com>
2024-04-30 17:34:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,69 @@
-// Disable right-click
-document.addEventListener('contextmenu', function(event) {
-    event.preventDefault();
-});
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const nav = document.querySelector('nav ul');
+  const navLinks = document.querySelectorAll('nav ul li a');
 
-// Disable Ctrl+Shift+I
-document.addEventListener('keydown', function(event) {
-    if (event.ctrlKey && event.shiftKey && event.keyCode === 73) {
-        event.preventDefault();
-    }
+  menuToggle.addEventListener('click', function () {
+    nav.classList.toggle('active');
+    menuToggle.classList.toggle('menu-open');
+  });
+
+  function scrollToSection(target) {
+    window.scrollTo({
+      top: target.offsetTop - 10,
+      behavior: 'smooth'
+    });
+  }
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+      if (targetSection) {
+        scrollToSection(targetSection);
+      } else {
+        console.error('Target section not found:', targetId);
+      }
+    });
+  });
+
+  const scrollPosition = localStorage.getItem('scrollPosition');
+  if (scrollPosition) {
+    window.scrollTo(0, scrollPosition);
+  }
+
+  window.addEventListener('scroll', function () {
+    localStorage.setItem('scrollPosition', window.pageYOffset);
+  });
+
+  const visitCookie = document.cookie
+    .split('; ')
+    .find(row => row.startsWith('visit='));
+  let visitCount = 0;
+  if (visitCookie) {
+    visitCount = parseInt(visitCookie.split('=')[1]) || 0;
+  }
+  visitCount++;
+  document.cookie = `visit=${visitCount}`;
+  var uservisits = "Visits: " + visitCount;
 });
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  var mybutton = document.getElementById("myBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.classList.add("show");
+    mybutton.classList.remove("hide");
+  } else {
+    mybutton.classList.remove("show");
+    mybutton.classList.add("hide");
+  }
+}
+
+function topFunction() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
