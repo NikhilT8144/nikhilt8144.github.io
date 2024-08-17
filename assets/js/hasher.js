b263d34628b894bd0b0556148a2eeb6f2f3db2bd
Nikhil Tiwari <87422608+NikhilT8144@users.noreply.github.com>
2024-08-17 18:24:43 +0530

    Update hasher.js

--- assets/js/hasher.js
+++ assets/js/hasher.js
@@ -1,32 +1,28 @@
+// hasher.js
+
 document.getElementById('hashForm').addEventListener('submit', function(e) {
     e.preventDefault();
 
     const hashInput = document.getElementById('hashInput').value;
     const hashType = document.getElementById('hashType').value;
-    const submitButton = e.target.querySelector('button[type="submit"]');
-
-    // Disable the submit button to prevent multiple submissions
-    submitButton.disabled = true;
 
     const data = {
         hash: hashInput,
         hashType: hashType
     };
 
-    fetch('https://nikhilt8144.great-site.net/hash/index.php', {
+    fetch('https://cors-anywhere.herokuapp.com/https://nikhilt8144.great-site.net/hash/index.php', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(data),
-        mode: 'cors' // Ensure CORS mode is set
-        // credentials: 'include' Include credentials like cookies if needed
+        mode: 'cors'
     })
     .then(response => response.json())
     .then(result => {
         if (result.success) {
             document.getElementById('result').innerHTML = `<p class="animate__animated animate__fadeIn">Hashed Result: ${result.hashedResult}</p>`;
-            document.getElementById('hashInput').value = '';  // Clear the input field
         } else {
             document.getElementById('result').innerHTML = '<p class="animate__animated animate__fadeIn">Error: Unable to hash input</p>';
         }
@@ -34,9 +30,5 @@ document.getElementById('hashForm').addEventListener('submit', function(e) {
     .catch(error => {
         console.error('Error:', error);
         document.getElementById('result').innerHTML = `<p class="animate__animated animate__fadeIn">Error: ${error.message}</p>`;
-    })
-    .finally(() => {
-        // Re-enable the submit button after processing is done
-        submitButton.disabled = false;
     });
 });
