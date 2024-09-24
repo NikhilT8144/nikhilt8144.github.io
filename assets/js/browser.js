bd42e69d66fcc503b0f970715c562801e356a1d1
Nikhil Tiwari <87422608+NikhilT8144@users.noreply.github.com>
2024-09-24 21:46:38 +0530

    Update browser.js

--- assets/js/browser.js
+++ assets/js/browser.js
@@ -1,8 +1,21 @@
-        document.getElementById('go-btn').addEventListener('click', function() {
-            var url = document.getElementById('url-input').value;
-            if (!url.startsWith('http')) {
-                url = 'http://' + url;  // Ensure http or https
-            }
-            var proxyUrl = 'https://nikhilt8144.serv00.net/proxy.php?url=' + encodeURIComponent(url);
-            document.getElementById('proxy-frame').src = proxyUrl;
-        });
+$(document).ready(function() {
+    $('#browse-btn').click(function() {
+        const urlToFetch = $('#url-input').val();
+        if (urlToFetch) {
+            const proxyURL = 'https://nikhilt8144.serv00.net/proxy.php';
+            $.get(proxyURL + '?url=' + encodeURIComponent(urlToFetch), function(data) {
+                $('#browser-content').html(data);
+            }).fail(function() {
+                $('#browser-content').html('<p>Error: Unable to load the page.</p>');
+            });
+        } else {
+            alert('Please enter a URL.');
+        }
+    });
+
+    $('#url-input').keypress(function(e) {
+        if (e.which === 13) {
+            $('#browse-btn').click();
+        }
+    });
+});
