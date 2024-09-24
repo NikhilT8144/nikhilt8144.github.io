68d21947cda7ea038648390d819b51b51a2a10b5
Nikhil Tiwari <87422608+NikhilT8144@users.noreply.github.com>
2024-09-24 22:03:06 +0530

    Update browser.js

--- assets/js/browser.js
+++ assets/js/browser.js
@@ -2,7 +2,12 @@ $(document).ready(function() {
     $('#browse-btn').click(function() {
         const urlToFetch = $('#url-input').val();
         if (urlToFetch) {
-            const proxyURL = 'https://nikhilt8144.serv00.net/proxy.php';
+            // Clear previous content and input
+            $('#browser-content').html('<p>Loading...</p>');
+            $('#url-input').val('');
+
+            // Make a request to the Serv00 backend
+            const proxyURL = 'https://nikhilt8144.serv00.net/proxy.php';  // Serv00 backend URL
             $.get(proxyURL + '?url=' + encodeURIComponent(urlToFetch), function(data) {
                 $('#browser-content').html(data);
             }).fail(function() {
@@ -13,6 +18,7 @@ $(document).ready(function() {
         }
     });
 
+    // Allow pressing 'Enter' to browse
     $('#url-input').keypress(function(e) {
         if (e.which === 13) {
             $('#browse-btn').click();
