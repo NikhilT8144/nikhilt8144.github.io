e47baf2b21f4166ae4cdc0274d49de297707101c
निखिल तिवारी <87422608+NikhilT8144@users.noreply.github.com>
2026-06-17 20:17:36 +0530

    Update 404.js

--- assets/js/404.js
+++ assets/js/404.js
@@ -1,1 +1,10 @@
-
+function load(url){
+  console.log(url);
+  if (url == "undefined"){
+    var site = "https://nikhilt8144.github.io/";
+  }else{
+    var site = "https://nikhilt8144.github.io/"+url;
+  }
+  console.log(site);
+  window.location.assign(site);
+};
